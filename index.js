762ac586df722b674e4f7b78ee30425d00a130fe
arunpravinnn <arunpravinap127@gmail.com>
2025-11-30 22:30:11 +0530

    Refactor

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ app.use(express.json());
 // Memory store for last 10 alerts
 let crashHistory = [];
 
-// Your Zoho channel webhook
+
 const WEBHOOK_URL =
   "https://cliq.zoho.in/api/v2/bots/firebasecrashlytics/message?zapikey=1001.aa68769fa1309d6a65cf9950486e8a86.ee4b3395450c6881c54ce4b392a3850d";
 
-// --------------------------------------------------
-// RECEIVE FIREBASE ALERTS (EventArc ➝ Cloud Run)
-// --------------------------------------------------
+
+// RECEIVE FIREBASE ALERTS (EventArc -> Cloud Run)
+
 app.post("/", async (req, res) => {
   console.log("Received Crashlytics event:");
   console.log(JSON.stringify(req.body, null, 2));
 
   try {
-    // Store alert in memory (keep last 10)
+
     crashHistory.unshift(req.body);
     crashHistory = crashHistory.slice(0, 10);
 
-    // Extract issue data
+
     const issue = req.body.payload?.issue;
 
     const msgText =
@@ -47,16 +47,12 @@ app.post("/", async (req, res) => {
   }
 });
 
-// --------------------------------------------------
-// GET LATEST ALERTS
-// --------------------------------------------------
+
 app.get("/api/latest", (req, res) => {
   res.json({ items: crashHistory });
 });
 
-// --------------------------------------------------
-// GET SINGLE ISSUE BY ID
-// --------------------------------------------------
+
 app.get("/api/issue/:id", (req, res) => {
   const issueId = req.params.id;
 
@@ -92,7 +88,7 @@ app.get("/api/issue/:id", (req, res) => {
   });
 });
 
-// --------------------------------------------------
+// for console logs
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Crashlytics forwarder running on port ${PORT}`);
